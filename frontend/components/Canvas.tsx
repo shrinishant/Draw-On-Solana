c0a485ef6c059a5be7392610066a2df6cc6a761b
shrinishant <nishantkumaranaspiringiitian@gmail.com>
2023-07-06 12:11:29 +0530

    rendering the pxels

--- frontend/components/Canvas.tsx
+++ frontend/components/Canvas.tsx
@@ -1,21 +1,25 @@
 import clsx from "clsx"
 import Pixel from "./Pixel"
-import {Program} from "@project-serum/anchor"
-import { useEffect } from "react"
+import {IdlAccounts, Program} from "@project-serum/anchor"
+import { useEffect, useState, useMemo } from "react"
 import { SolanaPrograms } from "@/idl/drawOnSolana"
 
 interface Props {
     program: Program<SolanaPrograms> | undefined
 }
 
+type PixelAccount = IdlAccounts<SolanaPrograms>['pixel']
+
 export default function Canvas({program} : Props) {
     const disabled = !program
+    const [fetchedPixels, setFetchedPixels] = useState<PixelAccount[]>([])
 
     const fetchPixels =async () => {
         if(program){
             try {
                 const pixels = await program.account.pixel.all()
-                console.log(pixels)
+                console.log("got the pixels", pixels)
+                setFetchedPixels(pixels.map(p => p.account))
             } catch (e) {
                 console.log(e)
             }
@@ -26,6 +30,15 @@ export default function Canvas({program} : Props) {
         fetchPixels()
     }, [program])
 
+    const pixelsMap = useMemo(() => {
+        const map: { [id: number]: PixelAccount } = {}
+        fetchedPixels.forEach(p => {
+            const id = p.posY * 100 + p.posX
+            map[id] = p
+        })
+        return map
+    }, [fetchedPixels])
+
     return (
         <div className={clsx(disabled && "opacity-25 cursor-not-allowed")}>
             <table className="border border-gray-300 table-fixed">
@@ -34,12 +47,15 @@ export default function Canvas({program} : Props) {
                         return (
                             <tr className="divide-x divide-gray-300" key={y}>
                                 {[...Array(100)].map((_, x) => {
+                                    const id = y * 100 + x
+                                    const pixelData = pixelsMap[id]
                                     return (
                                         <Pixel
                                             posX={x}
                                             posY={y}
                                             key={x}
                                             program={program}
+                                            pixelData={pixelData}
                                         />
                                     )
                                 })}
--- frontend/components/Pixel.tsx
+++ frontend/components/Pixel.tsx
@@ -1,15 +1,19 @@
-import { Program } from "@project-serum/anchor";
+import { Program, IdlAccounts } from "@project-serum/anchor";
 import { PublicKey, SystemProgram } from "@solana/web3.js";
 import { SolanaPrograms } from "@/idl/drawOnSolana"
+
+type PixelAccount = IdlAccounts<SolanaPrograms>['pixel']
 
 interface Props {
     posX: number,
     posY: number,
     program: Program<SolanaPrograms> | undefined
+    pixelData?: PixelAccount
 }
 
-export default function Pixel({ posX, posY, program }: Props) {
-    const color = "white"
+export default function Pixel({ posX, posY, program, pixelData }: Props) {
+    const {colR, colG, colB} = pixelData || {}
+    const color = pixelData ? `rgb(${colR}, ${colG}, ${colB})` : 'white'
 
     const getPixelAddress = () => {
         if(program !== undefined){
